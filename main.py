ff8fcc65b7a767a1ec28c617d581635c51c68462
Yuri-Campos <ygsoares20@gmail.com>
2024-02-24 21:20:17 -0300

    implementa o menu

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    pass
+def menu() -> int:
+    print("escolha uma opção: \n1-somar: \n2-subtrair: \n3-multiplicar: \n4-dividir: \nopção:")
+    escolha = int(input())
+    return escolha
 
+def main():
+    menu()
 
 if __name__ == "__main__":
     main()
