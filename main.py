01479edf96b644743642a1793e66223e93903a28
Yuri-Campos <ygsoares20@gmail.com>
2024-02-24 21:34:55 -0300

    implementa soma e lista de números

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
+def inserir_numeros():
+    controle = True
+    numeros = []
+    while(controle):
+        print('insira um número ou digite s para sair: \n')
+        num = input()
+        if num == 's' or num == 'S':
+            return numeros
+        numeros.append(int(num))
+        
+
+def soma():
+    numeros = inserir_numeros()
+    soma = 0
+    for i in numeros:
+        soma += i
+    print(f"\nResultado: {soma}")
+        
+def subtrai():
+    numeros = inserir_numeros()
+    pass
+def multiplica():
+    numeros = inserir_numeros()
+    pass
+def divide():
+    numeros = inserir_numeros()
+    pass
+
+
+
 def menu() -> int:
     print("escolha uma opção: \n1-somar: \n2-subtrair: \n3-multiplicar: \n4-dividir: \nopção:")
-    escolha = int(input())
+    escolha:int = int(input())
     return escolha
 
 def main():
-    menu()
+    escolha:int = menu()
+    match escolha:
+        case 1:
+            soma()
+        case 2:
+            subtrai()
+        case 3:
+            multiplica()
+        case 4:
+            divide()
 
 if __name__ == "__main__":
     main()
